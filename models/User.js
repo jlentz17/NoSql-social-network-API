28e542f3713894a9aacb39963e9e130b9cb1e332
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-26 17:33:00 -0700

    added more to User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,28 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model, Types } = require("mongoose")
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: ["Username is required!"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/]
+    },
+    thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought",
+        }
+      ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        }
+      ]
+})
